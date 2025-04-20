1919cea58127c72f1834c5c3f17e8e65c51c0853
Alan Zhang <initiator@alandevise.com>
2025-04-20 18:24:49 +0800

    上下两个模块都添加动画效果

---
+++ SwiftUiBaseStudy/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftUiBaseStudy
+//
+//  Created by Alan Zhang on 2025/4/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
