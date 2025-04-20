30e76eff14cd97762e7fe95a0cf893976252e72f
Alan Zhang <initiator@alandevise.com>
2025-04-20 17:54:54 +0800

    Profile 页面效果提交

---
+++ SwiftUiBaseStudy/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  SwiftUiBaseStudy
+//
+//  Created by Alan Zhang on 2025/4/20.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
