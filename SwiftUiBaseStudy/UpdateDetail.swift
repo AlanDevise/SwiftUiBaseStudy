875bb68ffd83c8d02e913bfdb439409a2fdb61ae
Alan Zhang <initiator@alandevise.com>
2025-05-10 17:17:51 +0800

    提交列表中跳转的代码逻辑

---
+++ SwiftUiBaseStudy/UpdateDetail.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateDetail.swift
+//  SwiftUiBaseStudy
+//
+//  Created by Alan Zhang on 2025/5/10.
+//
+
+import SwiftUI
+
+struct UpdateDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateDetail()
+}
---
+++ SwiftUiBaseStudy/UpdateList.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateList.swift
+//  SwiftUiBaseStudy
+//
+//  Created by Alan Zhang on 2025/5/10.
+//
+
+import SwiftUI
+
+struct UpdateList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateList()
+}
