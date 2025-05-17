196099be9469927ce5ce1910ff90676a241111f0
Alan Zhang <initiator@alandevise.com>
2025-05-17 16:21:40 +0800

    基础列表的删除新增功能实现

---
+++ SwiftUiBaseStudy/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  SwiftUiBaseStudy
+//
+//  Created by Alan Zhang on 2025/5/17.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
---
+++ SwiftUiBaseStudy/UpdateStore.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateStore.swift
+//  SwiftUiBaseStudy
+//
+//  Created by Alan Zhang on 2025/5/17.
+//
+
+import SwiftUI
+
+struct UpdateStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateStore()
+}
