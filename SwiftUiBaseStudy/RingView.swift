668be59a7ee1539114ccc56a01f7aa38a43a3186
Alan Zhang <initiator@alandevise.com>
2025-05-18 14:23:26 +0800

    进度环封装成组件代码提交

--- SwiftUiBaseStudy/RingView.swift
+++ SwiftUiBaseStudy/RingView.swift
@@ -8,38 +8,61 @@
 import SwiftUI
 
 struct RingView: View {
+    
+    var color1 = Color(#colorLiteral(red:0.2588235438,green:0.7568627596,blue:0.9686274529,alpha:1));
+    var color2 = Color(#colorLiteral(red:0.5568627715,green:0.3529411852,blue:0.9686274529,alpha:1));
+    var width: CGFloat = 44;
+    var height: CGFloat = 44;
+    var percent:Double = 82;
+    
     var body: some View {
         ZStack {
+            // 进度值
+            let progress = (100 - percent) / 100
+            // 比例系数
+            let multiplier = width / 44
             // 背景轨迹
             Circle()
                 .stroke(
-                    .black.opacity(0.1),
-                    style: StrokeStyle(lineWidth: 4,lineCap: .round)
+                    color1.opacity(0.1),
+                    style: StrokeStyle(
+                        lineWidth: 5 * multiplier,
+                        lineCap: .round
+                    )
                 )
-                .frame(width: 44, height: 44)
+                .frame(width: width, height: height)
             // 这个进度条通过外圈圆进行2D旋转90度，再3D以X轴旋转180度实现，
             // 通过控制trim中from参数的值可以控制从12点方向逆时针的空缺大小
             Circle()
-                .trim(from: 0.3, to: 1)
+                .trim(from: progress, to: 1)
                 .stroke(
-                    LinearGradient(gradient:Gradient(colors:[Color(#colorLiteral(red:0.2588235438,green:0.7568627596,blue:0.9686274529,alpha:1)),Color(#colorLiteral(red:0.5568627715,green:0.3529411852,blue:0.9686274529,alpha:1))]),startPoint:.topTrailing,endPoint:.bottomLeading),
+                    LinearGradient(
+                        gradient:Gradient(colors:[Color(color1),Color(color2)]),
+                        startPoint:.topTrailing,
+                        endPoint:.bottomLeading
+                    ),
                     style: StrokeStyle(lineWidth: 4,lineCap: .round)
                 )
-                .frame(width: 44, height: 44)
+                .frame(width: width, height: height)
                 .rotationEffect(Angle(degrees: 90))
                 .rotation3DEffect(
                     Angle(degrees: 180),
                     axis: (x: 1.0, y: 0.0, z: 0.0)
                 )
-                .shadow(color: Color(#colorLiteral(red:0.568627715, green:0.3529411852,blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 3, x: 0, y: 3)
-            // 数值文字
-            Text("82%")
-                .font(.subheadline)
+                .shadow(
+                    color: color1.opacity(0.3),
+                    radius: 3 * multiplier,
+                    x: 0,
+                    y: 3 * multiplier
+                )
+            // 数值文字（Double）类型的percent可能有小数点，用Int方法转换成整数显示
+            Text("\(Int(percent))%")
+                .font(.system(size: 14*multiplier))
                 .bold()
         }
     }
 }
 
 #Preview {
-    RingView()
+    RingView(color1: .red,width: 88,height: 88)
 }
