e3181a1bfac501a7631a515128cee692fd32b9b1
Alan Zhang <initiator@alandevise.com>
2025-05-18 13:48:09 +0800

    提交进度圆框的代码

---
+++ SwiftUiBaseStudy/RingView.swift
@@ -0,0 +1,18 @@
+//
+//  RingView.swift
+//  SwiftUiBaseStudy
+//
+//  Created by Alan Zhang on 2025/5/18.
+//
+
+import SwiftUI
+
+struct RingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RingView()
+}
