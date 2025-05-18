a1167338306f9dfc9920d36ef35ae8bb545f03de
Alan Zhang <initiator@alandevise.com>
2025-05-18 15:16:57 +0800

    提交RingView组件应用到其他页面的代码逻辑

--- SwiftUiBaseStudy/readme.mk
+++
@@ -1,6 +0,0 @@
-## part2-01 Ring Progress Circle
-颜色
-```swift
-LinearGradient(gradient:Gradient(co1ors:[Color(#colorLiteral(red:0.2588235438,green:0.7568627596,blue:0.9686274529,alpha:1)),Color(#co1orLiteral(red:0.5568627715,green:0.3529411852,blue:0.9686274529,alpha:1))1),startPoint:.topTrai1ing,endPoint:.bottomLeading),style:StrokeStyle(lineWidth:5,lineCap:.round)
-Color(#colorLiteral(red:0.568627715, green:0.3529411852,blue: 0.9686274529, alpha: 1)).opacity(0.3)
-```
