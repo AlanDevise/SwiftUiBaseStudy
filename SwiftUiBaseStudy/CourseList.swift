adcdee5fb8b99c74b5b65640ac49b527275114d3
Alan Zhang <initiator@alandevise.com>
2025-05-25 21:05:08 +0800

    提交详情页的放大缩小逻辑

---
+++ SwiftUiBaseStudy/CourseList.swift
@@ -0,0 +1,18 @@
+//
+//  CourseList.swift
+//  SwiftUiBaseStudy
+//
+//  Created by Alan Zhang on 2025/5/25.
+//
+
+import SwiftUI
+
+struct CourseList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseList()
+}
